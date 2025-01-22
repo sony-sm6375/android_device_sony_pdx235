9f843b85cb26ad5870d6939769168956702941ca
LuK1337 <priv.luk@gmail.com>
2025-01-22 15:07:18 +0100

    pdx235: Set default bluetooth name
    Change-Id: Iecbabba448d0b2b31ca2e1399301799cd546492c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,5 +8,8 @@ include device/sony/sm6375-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/sony/pdx235
 
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Include the proprietary files BoardConfig.
 include vendor/sony/pdx235/BoardConfigVendor.mk
